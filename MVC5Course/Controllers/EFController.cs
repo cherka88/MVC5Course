fe3d02da768404dba40581dfe45edaf80b818656
cherka88 <toyo2054@gmail.com>
2017-05-07 11:59:19 +0800

    16 練習建立一個 EFController 並練習完整的 Entity Framework 資料存取方法 (CRUD)

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+        // GET: EF
+        public ActionResult Index()
+        {
+           
+            var all = db.Product.AsQueryable();
+
+            var data = all.Where(p => p.Active == true && p.ProductName.Contains("Black"));
+
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(prod);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+                item.ProductName = prod.ProductName;
+                item.Price = prod.Price;
+                item.Stock = prod.Stock;
+                item.Active = prod.Active;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(prod);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = db.Product.Find(id);
+            db.Product.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Details(int? id)
+        {
+            var product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
