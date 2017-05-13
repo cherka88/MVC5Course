09890005a28a0f65ec3d0af6bc0452ca20b69569
lin profiteer <terence@linde-MacBook-Pro.local>
2017-05-13 21:56:26 +0800

    ClientsContoller/BatchUpdate 批次更新

---
+++ MVC5Course/Controllers/ClientsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        public void GetClient()
+        {
+            var client = db.Client.Include(c => c.Occupation).Take(10);
+            ViewData.Model = client;
+        }
+        
+        public ActionResult BatchUpdate()
+        {
+            GetClient();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BatchUpdate(ClientBatchUpdateVM[] items)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var c = db.Client.Find(item.ClientId);
+                    c.FirstName = item.FirstName;
+                    c.MiddleName = item.MiddleName;
+                    c.LastName = item.LastName;
+                }
+                db.SaveChanges();
+                return RedirectToAction("BatchUpdate");
+            }
+            GetClient();
+            return View();
+        }
+       
+    }
+}
